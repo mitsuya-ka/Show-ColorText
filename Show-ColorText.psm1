3f721847359d792dad76da7538a857de051a98e5
mitsuya-ka <69183133+mitsuya-ka@users.noreply.github.com>
2021-12-24 14:02:34 +0900

    Create Show-ColorText.psm1

---
+++ Show-ColorText.psm1
@@ -0,0 +1,42 @@
+function Show-ColorText {
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipeLineByPropertyName = $true, Position = 0)]
+		[String] $Line,
+		[ValidateSet('Black', 'BrightBlack', 'White', 'BrightWhite', 'Red', 'BrightRed', 'Magenta', 'BrightMagenta', `
+				'Blue', 'BrightBlue', 'Cyan', 'BrightCyan', 'Green', 'BrightGreen', 'Yellow', 'BrightYellow')]
+		[String] $Color,
+		[string] $Word,
+		[switch] $CaseSensitive
+	)
+	begin {
+		if ( $CaseSensitive ) {
+			$sw = "Ordinal"
+		}
+		else {
+			$sw = "OrdinalIgnoreCase"
+		}
+	}
+	process {
+		if ( $PSStyle ) {
+			if ( $Word ) {
+				$index = $Line.IndexOf($Word, [System.StringComparison]::$sw)
+				while ($index -ge 0) {
+					$str += $Line.Substring(0, $index)
+					$str += $PSStyle.Foreground.$Color + $Line.Substring($index, $Word.Length) + $PSStyle.Reset
+					$used = $Word.Length + $index
+					$remain = $Line.Length - $used
+					$Line = $Line.Substring($used, $remain)
+					$index = $Line.IndexOf($Word, [System.StringComparison]::$sw)
+				}
+				$str += $Line
+				$str
+			}
+			else {
+				$PSStyle.Foreground.$Color + $Line + $PSStyle.Reset
+			}
+		}
+		else {
+			$Line
+		}
+	}
+}
